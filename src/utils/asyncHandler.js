f3f2366ab48ab1421136bb56a9b1d9fb406f09a3
Anishashettty <shettyanisha2004@gmail.com>
2025-07-14 17:41:52 +0530

    util code usage

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+
+// This utility function is used to handle asynchronous operations in Express.js routes.
+// It wraps the request handler and catches any errors that occur, passing them to the next middleware.
+ 
+
+const asyncHandler=(requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err));
+    }
+}
+
+export {  asyncHandler}
+
+//-------------try and catch method------
+// const asyncHandler=(fn)=>async(req,res,next)=>{
+//     try {
+//         await fn(req, res, next);
+//     } catch (error) {
+//         res.status(error.code || 500).json({ message: error.message || "Internal Server Error" });
+//         console.error("Error in asyncHandler:", error);
+//         res.status(500).json({ message: "Internal Server Error" });
+//     }
+// }
